bcae9c54d9ac8ac17f552fb94836869c8094c0dd
ja0ck5 <975117619@qq.com>
2017-11-07 23:32:39 +0800

    flask python

---
+++ flaskPy.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+@app.route('/index/<user>',methods=['POST','GET'])
+def hello_world(user):
+    # aint = 1
+    # astr = '1'
+    # result =  aint + astr
+    # return 'Hello World!'
+    return 'Hello %s' %user
+
+if __name__ == '__main__':
+    app.run(debug=True)
