2dcb172090a3a8bbdf43b978c21e0d9a3a78020a
ja0ck5 <975117619@qq.com>
2017-11-08 17:33:41 +0800

    requests

--- flaskPy.py
+++ flaskPy.py
@@ -1,6 +1,30 @@
-from flask import Flask,request
+import base64
+import random
+import time
+
+from flask import Flask, request
 
 app = Flask(__name__)
+
+users = {
+    'name': ['123456']
+}
+
+
+def get_token(uid):
+    token = base64.b64encode(':'.join([str(uid), str(random.random()), str(time.time() + 7200)]))
+    users[uid].append(token)
+    return token
+
+
+def verify_token(token):
+    _token = base64.b64decode(token)
+    if not users.get(_token.split(':')[0])[-1] == token:
+        return -1
+    if float(_token.split(':')[-1]) >= time.time():
+        return 1
+    else:
+        return 0
 
 
 # @app.route('/')
@@ -12,6 +36,24 @@ def hello_world(user):
     # return 'Hello World!'
     print request.headers
     return 'Hello %s' % user
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    uid, pw = base64.b64decode(request.headers['Authorization'].split(' ')[-1]).split(':')
+    if users.get(uid)[0] == pw:
+        return get_token(uid)
+    else:
+        return 'error'
+
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    token = request.args.get('token')
+    if 1 == verify_token(token):
+        return 'OK'
+    else:
+        return 'error'
 
 
 if __name__ == '__main__':
--- reqeusts_t.py
+++ reqeusts_t.py
@@ -3,7 +3,10 @@
 
 import requests
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-r = requests.get('http://127.0.0.1:5000/index/user', headers=headers)
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+# r = requests.get('http://127.0.0.1:5000/index/user', headers=headers)
+# r = requests.get('http://127.0.0.1:5000/login', auth=('name','123456'))
+token = 'bmFtZTowLjAyNDEwNzk0NTAyMjc6MTUxMDEzOTYzNy40Mg=='
+r = requests.get('http://127.0.0.1:5000/test', params={'token': token})
 print r.text
